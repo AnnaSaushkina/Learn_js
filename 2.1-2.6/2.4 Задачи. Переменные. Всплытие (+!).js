c482a4eef5c182e659b2ab987aa621e597797b12
Anna Saushkina <saushkinanna@gmail.com>
2024-11-05 21:59:00 +0300

    2.12

--- 2.1-2.6/2.4 Задачи. Переменные. Всплытие (+!).js
+++ 2.1-2.6/2.4 Задачи. Переменные. Всплытие (+!).js
@@ -1,4 +1,99 @@
-// Задача 1: Объявление переменных
+// ▎Задача 1: Объявление переменной
+
+console.log(a); // Что выведет этот код?
+var a = 5; // underfined // Обращение к переменной до ее создания
+
+
+
+// ▎Задача 2: Функции и hoisting
+
+console.log(myFunction()); // Что выведет этот код?
+
+function myFunction() {
+    return "Hello, World!";
+} // Выводит "Hello, World!" // Можно обращаться к функции до ее объявления
+
+
+
+// ▎Задача 3: let и const
+
+console.log(b); // Что выведет этот код?
+let b = 10; //  ошибка
+
+// Вопрос: Какое значение будет выведено в консоль? Почему результат отличается от случая с var?
+// Var создает пустую переменную в глобальной области видимости
+
+
+
+// ▎Задача 4: Область видимости
+
+console.log(c); // Что выведет этот код? // undefined
+var c = 15;
+
+function test() {
+    console.log(c); // Что выведет здесь? // undefined
+    var c = 20;
+}
+
+test();
+
+console.log(c); // Что выведет этот код? // ошибка. код прервется
+let c = 15;
+
+function test() {
+    console.log(c); 
+    let c = 20;
+}
+
+test();
+
+// Вопрос: Какое значение будет выведено в каждом случае и почему?
+//  Первый вывод: undefined потому что переменная var поднимается. 
+// Второй вывод:  внутри функции работает аналогично переменной:  undefined потому что переменная var поднимается. 
+// Третий вывод: let не инициализирует переменную после обращения к ней. выкидывает ошибку
+
+
+// ▎Задача 5: Функциональные выражения
+
+console.log(d()); // Что выведет этот код?
+
+var d = function() {
+    return "Function Expression";
+}; // ошибка
+
+// Вопрос: Почему возникает ошибка или какое значение будет выведено?
+// Фнкциональное выражение к коорому обращаются до объявления выдает ошибку
+
+// ▎Задача 6: Hoisting и IIFE
+
+(function() {
+    console.log(e);
+    var e = "IIFE";
+})();
+
+// Вопрос: Какое значение будет выведено в консоль и почему?
+//undefined // обращаемся к переменной до ее объявления
+
+
+
+// ▎Задача 7: Смешение типов
+
+console.log(f); // Что выведет этот код? //undefined // обращаемся к объявленной но непроинициализированной переменной
+var f = "first";
+console.log(f); // Что выведет здесь? // first // обращаемся к проинициаиализированному значению переменной
+var f = "second"; // не обращаемся к переназначенному значению
+
+
+
+// ▎Задача 8: Проблемы с hoisting
+
+console.log(g); // Что выведет этот код? // undefined
+g = "Global"; // Попытка присвоить значение // присвоено значение, к которому мы не успели обратиться
+var g;
+
+
+
+// Задача 9: Объявление переменных
 
 // Напишите функцию declareVariables, которая объявляет переменные с использованием var, let и const. Проверьте, какие из них доступны в разных областях видимости.
 
@@ -8,40 +103,41 @@ function declareVariables() {
         let letVariable = "I am let";
         const constVariable = "I am const";
     }
-
-    console.log(varVariable); // Что выведет?
-    console.log(letVariable); // Что выведет?
-    console.log(constVariable); // Что выведет?
+    console.log(varVariable); // Что выведет? // "I am var"
+    console.log(letVariable); // Что выведет? // остановит программу с ошибкой
+    console.log(constVariable);
 }
-
-// Вызовите функцию
 declareVariables();
+// Если инициализация переменной и обращение к ней в передлах одного блока - все ок (для всех var let const)
+// Если обращение к проиниц. переменной за пределами блока то ок только для var.  Обращение в let и const выдает ошибку
 
 
-// ▎Задача 2: Всплытие переменных
+// ▎Задача 10: Всплытие переменных
 
 // Создайте функцию hoistingExample, которая демонстрирует, как работает всплытие переменных. Попробуйте вывести значение переменной до ее объявления.
 
 function hoistingExample() {
-    console.log(hoistedVar); // Что выведет?
+    console.log(hoistedVar); // Что выведет? // undefined
     var hoistedVar = "I am hoisted";
+  
+    console.log(hoistedVar); // Что выведет? // "I am hoisted"
+  }
+  
+  // Вызовите функцию
+  hoistingExample();
 
-    console.log(hoistedVar); // Что выведет?
-}
-
-// Вызовите функцию
-hoistingExample();
 
 
-// ▎Задача 3: Всплытие функций
+// ▎Задача 11: Всплытие функций// ВЕРНУТЬСЯ
 
 // Напишите функцию hoistedFunction, которая демонстрирует всплытие функций. Попробуйте вызвать функцию до ее объявления.
+
+hoistedFunction(); // Что выведет? // "This function is hoisted!"
 
 function hoistedFunction() {
     console.log("This function is hoisted!");
 }
 
-hoistedFunction(); // Что выведет?
 
 // Также попробуйте вызвать анонимную функцию
 try {
@@ -58,55 +154,56 @@ var hoistedAnonymousFunction = function() {
 hoistedAnonymousFunction(); // Что выведет?
 
 
-// ▎Задача 4: Область видимости переменных
+
+// ▎Задача 12: Область видимости переменных
 
 // Создайте функцию scopeExample, которая демонстрирует разницу между областью видимости переменных, объявленных с помощью var, let и const.
+
 
 function scopeExample() {
     var a = 1;
     let b = 2;
     const c = 3;
-
+  
     if (true) {
         var a = 4; // Перекрывает предыдущий var a
         let b = 5; // Создает новую область видимости для let b
         const c = 6; // Создает новую область видимости для const c
-
-        console.log(a); // Что выведет?
-        console.log(b); // Что выведет?
-        console.log(c); // Что выведет?
+  
+        console.log(a); // 4
+        console.log(b); // 5
+        console.log(c); // 6
     }
+  
+    console.log(a); // 4
+    console.log(b); // 2
+    console.log(c); // 3
+  }
+  
+  // Вызовите функцию
+  scopeExample();
 
-    console.log(a); // Что выведет?
-    console.log(b); // Что выведет?
-    console.log(c); // Что выведет?
-}
-
-// Вызовите функцию
-scopeExample();
 
 
-// ▎Задача 5: Замыкания и всплытие
+// ▎Задача 13: Замыкания и всплытие
 
 // Напишите функцию closureExample, которая создает замыкание. Проверьте, как всплытие влияет на доступ к переменным внутри замыкания.
-
 function closureExample() {
     var outerVar = "I am outside!";
-
+  
     function innerFunction() {
-        console.log(outerVar); // Что выведет?
+        console.log(outerVar); // undefined
         var innerVar = "I am inside!";
     }
-
+  
     innerFunction();
-    console.log(innerVar); // Что выведет? (доступен ли innerVar здесь?)
-}
+    console.log(innerVar); // Что выведет? ошибка
+  }
+  
+  // Вызовите функцию
+  closureExample();
 
-// Вызовите функцию
-closureExample();
-
-
-// ▎Задача 6: Переменные в циклах
+// ▎Задача 14: Переменные в циклах
 
 // Создайте функцию loopVariables, которая демонстрирует поведение переменных в цикле с использованием var, let и const.
 
@@ -128,7 +225,7 @@ function loopVariables() {
 loopVariables();
 
 
-// ▎Задача 7: Разница между let и var
+// ▎Задача 15: Разница между let и var
 
 // Напишите функцию letVsVar, которая демонстрирует разницу между let и var в контексте цикла.
 
---
+++ 2.7.2.12/2.12 Задачи. Операторы нулевого слияния и присваивания (+!).js
@@ -0,0 +1,69 @@
+
+// Задача 1: Что выведет этот код?
+// Что выведет код ниже?
+console.log(undefined ?? NaN ?? null ?? "" ?? " ") // NaN
+
+
+// Задача 2: Какой будет результат выполнения этого кода?
+// Что будет выведено в итоге?
+
+let city = null;
+
+city ??= "Берлин";
+city ??= null;
+city ??= "Кёльн";
+city ??= "Гамбург";
+
+console.log(city); //"Берлин"
+
+
+// Задача 3: Перепишите код используя операторы ??, ??=
+// Перепишите этот код используя операторы нулевого слияния и присваивания.
+
+let num1 = 10,
+    num2 = 20,
+    result;
+
+if (result === null || result === undefined) {
+  if (num1 !== null && num1 !== undefined) {
+    result = num1;
+  } else {
+    result = num2;
+  }
+}
+
+// Задача 4: Проверка значений
+
+// Создайте функцию getValue, которая принимает два параметра: value1 и value2. Функция должна возвращать value1, если оно не равно null или undefined, в противном случае возвращать value2.
+
+function getValue(value1, value2) {
+    // Ваш код здесь
+}
+
+// Примеры вызова функции
+console.log(getValue(null, "default")); // "default"
+console.log(getValue(undefined, "default")); // "default"
+console.log(getValue("Hello", "default")); // "Hello"
+
+
+// ▎Задача 5: Установка значений по умолчанию
+
+// Напишите функцию setDefault, которая принимает объект и устанавливает значение по умолчанию для свойства name. Если name отсутствует или равно null, то оно должно быть установлено в "Anonymous".
+
+function setDefault(obj) {
+    // Ваш код здесь
+}
+
+// Примеры вызова функции
+let user1 = { name: null };
+let user2 = { name: "Alice" };
+
+setDefault(user1);
+setDefault(user2);
+
+console.log(user1.name); // "Anonymous"
+console.log(user2.name); // "Alice"
+
+
+
+
