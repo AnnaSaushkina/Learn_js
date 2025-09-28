c6579a2f6d836b4dda91ce9a24fea5fdc78f2e19
Anna Saushkina <saushkinanna@gmail.com>
2025-09-28 20:53:38 +0300

    add: 2.17 задачи на стрелочные функции

---
+++ 2.17/2.17 стрелочные функции.js
@@ -0,0 +1,83 @@
+
+let ask2 = (question, yes, no) => {
+  if (confirm(question)) yes() 
+  else no();
+}
+
+ask2(
+    'Вы согласны2?',
+    function() { console.log('Мы согласились')},
+    function() { console.log('Мы отменились');}
+)
+
+
+// Напиши функцию, которая принимает радиус и возвращает площадь круга
+let calculateCircleArea = (radius) => {
+  return radius *= radius * 3.14
+}
+
+console.log(calculateCircleArea(5)); // 78.5398...
+console.log(calculateCircleArea(10)); // 314.159...-
+
+
+// Напиши функцию, которая проверяет, является ли строка палиндромом
+let isPalindrome = (str) => {
+  const noSpacesStr = str.replaceAll(' ', '').toLowerCase().split("");
+  
+  const strLetters = noSpacesStr.toString()
+  const reversedString = Array.from(noSpacesStr).reverse().toString()
+    
+   return (strLetters === reversedString) ? true : false;
+}
+
+console.log(isPalindrome("топот")); // true
+console.log(isPalindrome("привет")); // false
+console.log(isPalindrome("А роза упала на лапу Азора")); // true
+
+
+// Напиши функцию, которая фильтрует массив чисел, оставляя только четные
+let filterEvenNumbers = (numbers) => {
+  let newArray = numbers.filter((number) => number % 2 == 0)
+  return newArray
+ }
+ 
+ console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+ console.log(filterEvenNumbers([1, 3, 5])); // []
+
+
+ // Напиши функцию, которая находит максимальное число в массиве
+ let findMaxNumber = (numbers) => {
+  return result = Math.max.apply(null, numbers)
+}
+
+console.log(findMaxNumber([1, 5, 2, 9, 3])); // 9
+console.log(findMaxNumber([-10, -5, -1])); // -1
+
+
+// Параметры по умолчанию
+// Напиши функцию formatMessage(text, prefix = "Info:", suffix = "!")
+// которая возвращает отформатированное сообщение
+let formatMessage = (text, prefix = 'Info:', suffix = '!') => {
+  return prefix + text + suffix
+}
+
+console.log(formatMessage('Некое сообщение')) //"Info:Некое сообщение!"
+
+
+// Rest параметры
+// Напиши функцию calculateAverage(...numbers), которая возвращает
+// среднее арифметическое всех переданных чисел
+
+let calculateAverage = (...numbers) => {
+
+  let totalSum = 0;
+  for (const nums of numbers) {
+    totalSum += nums;
+  }
+  
+  return totalSum / numbers.length
+}
+
+console.log(calculateAverage(2, 5, 4))
+
+
