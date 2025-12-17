f813a6ec15c76aa161865e4521773e6ae348d2c4
Anna Saushkina <111707683+AnnaSaushkina@users.noreply.github.com>
2025-12-18 02:19:56 +0400

    4.8 конспект

--- 4.Объекты/4.7-4.8/4.7.js
+++ 4.Объекты/4.7-4.8/4.7.js
@@ -18,3 +18,53 @@ let counter = {
 console.log(counter.method1())
 console.log(counter.method1())
 
+
+
+
+// 2. Создать логгер с историей который:
+// Хранит историю всех вызовов методов через Symbol
+// Имеет методы: log(message), warn(message), error(message)
+// Каждый вызов добавляет запись в историю
+// Метод getHistory() возвращает всю историю
+const HISTORY = Symbol('history');
+
+const logger = {
+    [HISTORY]: [],
+
+    log(msg) {
+        let entry = {
+            type: 'log',
+            message: msg,
+        };
+
+        this[HISTORY].push(entry)
+
+    },
+
+    warn(msg) {
+        let entry = {
+            type: 'warn',
+            message: msg
+        };
+
+        this[HISTORY].push(entry)
+    },
+
+    error(msg) {
+        let entry = {
+            type: 'error',
+            message: msg
+        };
+
+        this[HISTORY].push(entry)
+    },
+
+
+    getHistory() {
+        return [...this[HISTORY]]
+    }
+};
+
+logger.log("Привет");
+logger.warn("Внимание!");
+console.log(logger.getHistory());
